b5bb6a8148235788440c585810f6833887853d29
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-06-04 07:50:08 +0900

    Update init.lua

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,1 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
+-- キーマッピング
+vim.g.mapleader = " "
+
+-- カラースキーム
+vim.cmd("colorscheme desert")
