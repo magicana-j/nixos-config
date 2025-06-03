92955396e0872fed6f848b08a355a52290e9d384
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-06-04 07:45:46 +0900

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.amuharai = home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgs;
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+}
