cca0838cf7a0cea0f6ad3e40422f8c41ae6256c7
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-11-16 07:56:22 +0900

    Remove Neovim configuration
    Removed Neovim configuration from programs.nix.

--- modules/programs.nix
+++ modules/programs.nix
@@ -15,28 +15,6 @@
     set tabsize 2
   '';
 
-  # Neovim
-  programs.neovim = {
-    enable = true;
-    configure = {
-      customRC = ''
-set encoding=utf-8
-set number
-set relativenumber
-set autoindent
-set smartindent
-set expandtab
-set tabstop=2
-set shiftwidth=2
-set listchars=tab:→\ ,space:·,nbsp:☣,trail:•,eol:¶,precedes:«,extends:»
-if &diff
-  colorscheme blue
-endif
-'';
-      packages.myVimPackage = with pkgs.vimPlugins; {
-        start = [ ctrlp ];
-      };
-    };
   };
 
   # Direnv for project-specific environments
@@ -45,8 +23,4 @@ endif
     nix-direnv.enable = true;
   };
 
-  # Starship prompt
-  programs.starship = {
-    enable = true;
-  };
 }
