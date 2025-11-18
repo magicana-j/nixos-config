1da3a0660b1c3be32fc9ca3d4f4ceb98a1a92fa3
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-11-18 15:37:58 +0900

    home.nix を更新

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 {
   home.username = "amuharai";
   home.homeDirectory = "/home/amuharai";
+  home.stateVersion = "25.05";
 
   programs.home-manager.enable = true;
 
@@ -48,9 +49,6 @@
     libreoffice-fresh
   ];
 
-  imports = [
-    ../../modules/neovim.nix
-  ];
   # 例: dotfiles的な設定
 
   # ここに fcitx5, terminal, editor, etc…を足していく
