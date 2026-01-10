91843c6da31b28a1851a8ae6abbc82515af2cca2
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2026-01-10 17:31:45 +0900

    Add files via upload

--- config.d/deskop.nix
+++ config.d/deskop.nix
@@ -5,11 +5,35 @@
   services.xserver.enable = true;
 
   # ディスプレイマネージャー: SDDM (ログイン画面)
-#  services.displayManager.sddm.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "breeze";
+    settings = {
+      Theme = {
+        Background = "/etc/nixos/wallpaper.png";
+        Blur = true;
+      };
+    };
+  };
+  
+#  services.xserver.displayManager.lightdm.enable = true;
 
   # デスクトップ環境: Cinnamon
-  services.xserver.desktopManager.cinnamon.enable = true;
+#  services.xserver.desktopManager.cinnamon.enable = true;
+
+  # デスクトップ環境: KDE Plasma 6
+  services.desktopManager.plasma6.enable = true;
+
+  # PAMでログイン時にkeyringを解錠（SDDM + ログイン両方）
+  security.pam.services.sddm.enableGnomeKeyring = true;
+  security.pam.services.login.enableGnomeKeyring = true;
+
+  # KWallet側のPAMは切る（両立させると「どっち解錠？」になってうるさい）
+  security.pam.services.sddm.kwallet.enable = lib.mkForce false;
+  security.pam.services.login.kwallet.enable = lib.mkForce false;
+
+  # GNOME Keyring (パスワード管理システム)
+  services.gnome.gnome-keyring.enable = true;
 
   # Wayland関連
   # Xwaylandを有効化 (X11アプリをWayland上で動かすための互換レイヤー)
@@ -33,6 +57,4 @@
     enable32Bit = true;
   };
 
-  # GNOME Keyring (パスワード管理システム)
-  services.gnome.gnome-keyring.enable = true;
 }
